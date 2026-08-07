8633e5e2353d83725e21b93473f3ac4f2a812563
M Hammad <mhammad255503531@gmail.com>
2026-08-07 11:10:53 +0500

    Complete Week 1 Day 1 ML environment setup

---
+++ test_environment.py
@@ -0,0 +1,10 @@
+import numpy
+import pandas
+import sklearn
+import xgboost
+
+print("NumPy:", numpy.__version__)
+print("Pandas:", pandas.__version__)
+print("Scikit-learn:", sklearn.__version__)
+print("XGBoost:", xgboost.__version__)
+print("ML environment is ready!")
